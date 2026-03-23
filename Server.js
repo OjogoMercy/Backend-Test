28d5c175989e62f62931b6d6a664b89612141d40
OjogoMercy <mercyojogo@gmail.com>
2026-03-23 13:23:54 +0100

    added logging for simple events

--- Server.js
+++ Server.js
@@ -1,24 +1,9 @@
-// Day 1 reading files in Node.js
 const fsPromises = require("fs").promises;
 const path = require("path");
+const {v4: uuid}= require('uuid')
+
+const childId = uuid();
+console.log(childId)
 
 
 
-// fs.readFile(path.join(__dirname, "text.txt"), "utf8", (error, data) => {
-//   if (error) throw error;
-//   console.log(data);
-// });
-
-// fs.appendFile(
-//   path.join(__dirname, "Reply.txt"),
-//   " Nice to meet you  ",
-//   (error) => {
-//     if (error) throw error;
-//     console.log("Here is a message to see if this works ");
-//   },
-// );
-
-
-// console.log("help me im tired already ...");
-
-//
