d48241b7284f6e29fb57b79be4187d004da336a5
OjogoMercy <mercyojogo@gmail.com>
2026-03-24 13:07:33 +0100

    server handling

--- Server.js
+++ Server.js
@@ -1,24 +1,24 @@
 const fsPromises = require("fs").promises;
 const path = require("path");
 const http = require("http");
-const fs = require('fs')
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3000;
 // to listen and check different file type s
 const serveFile = async (response, filePath, contentType) => {
   try {
-    const data = await fsPromises.readFile(filePath, !contentType.includes('image') ? 'utf8' : '');
+    const isImage = contentType.startsWith("image/");
+    const data = await fsPromises.readFile(filePath, isImage ? null : "utf8");
     response.writeHead(200, { "Content-Type": contentType });
     response.end(data);
   } catch (error) {
     response.writeHead(404, { "Content-Type": "text/plain" });
-    response.end("404 File was not found ");
+    response.end("404 - File was not found");
   }
 };
 
 const server = http.createServer((request, response) => {
   console.log(request.url, request.method);
-  const extension = path.extname(request.url);
 
   // to normalise the filepath
   let filePath;
@@ -38,17 +38,14 @@ const server = http.createServer((request, response) => {
       filePath = path.join(__dirname, "public", "app.js");
       serveFile(response, filePath, "application/javascript");
       break;
-      case "Views/index.html": 
-      filePath = path.join(__dirname,"Views", "index.html");
-      serveFile(response,filePath,"image/jpeg");
+    case "/assets/image.png":
+      filePath = path.join(__dirname, "assets", "image.png");
+      serveFile(response, filePath, "image/png");
       break;
     default:
       response.writeHead(404, { "Content-Type": "text/plain" });
       response.end("404 - Not Found");
   }
 });
-
-
-
 
 server.listen(PORT, () => console.log(`server listening on PORT ${PORT}`));
