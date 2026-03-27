dd9ee064140db1ead9957af6b597ca4eb2107a05
OjogoMercy <mercyojogo@gmail.com>
2026-03-27 13:11:55 +0100

    basic express middleware added

--- Server.js
+++ Server.js
@@ -1,23 +1,25 @@
-const http = require("http");
 const path = require("path");
-const fsPromises = require("fs").promises;
-const fs = require("fs");
 const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+// to handle form data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 // to listen and check different file type s
-const serveFile = async (response, filePath, contentType) => {
-  try {
-    const isImage = contentType.startsWith("image/");
-    const data = await fsPromises.readFile(filePath, isImage ? null : "utf8");
-    response.writeHead(200, { "Content-Type": contentType });
-    response.end(data);
-  } catch (error) {
-    response.writeHead(404, { "Content-Type": "text/plain" });
-    response.end("404 - File was not found");
-  }
-};
+
+// const serveFile = async (response, filePath, contentType) => {
+//   try {
+//     const data = await fsPromises.readFile(filePath);
+//     response.writeHead(200, { "Content-Type": contentType });
+//     response.end(data);
+//   } catch (error) {
+//     response.writeHead(404, { "Content-Type": "text/plain" });
+//     response.end("404 - File was not found");
+//   }
+// };
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "Views", "index.html"));
 });
