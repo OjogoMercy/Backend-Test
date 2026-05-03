b8bcf3889430c35d0af1816abb4216e87d67610c
OjogoMercy <mercyojogo@gmail.com>
2026-05-03 21:51:19 +0100

    feat: add Immunisation route with validation and error handling

---
+++ src/routes/immunisation.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const prisma = require("./prismaClient");
+const verifyToken = require("./verifyToken")
+
+app.post("/Immunisation", verifyToken, async (req, res) => {
+  try {
+  const { vaccineName, dueDate, childId } = req.body;
+     if (!vaccineName || !dueDate || !childId) {
+      return res.status(400).json({ message: "Please fill in all fields" });
+    }
+    const child = await prisma.child.findFirst({
+      where: { id: childId, userId: req.user.userId },
+    });
+    if (!child) {
+      return res.status(404).json({ message: "child not found" });
+    }   
+    const parsedDueDate = new Date(dueDate);
+    const newImmunisation = await prisma.immunisation.create({
+      data: {  vaccineName,
+      dueDate: parsedDueDate,
+      childId: childId,}
+    });
+    return res.status(201).json({
+      message: "Immunisation created successfully",
+      immunisation: newImmunisation,
+    });
+  } catch (error) {
+    console.error("error messsage", error);
+    return res.status(500).json({ message: "server error" });
+  }
+});
