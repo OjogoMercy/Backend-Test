560c4cef67a67f12228eb3202bedda94a274c88c
OjogoMercy <mercyojogo@gmail.com>
2026-05-26 20:35:45 +0100

    feat: add ws dependency and configure Prisma with WebSocket support

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,32 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig, Pool } from "@neondatabase/serverless";
+import { PrismaClient } from "@prisma/client";
 
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env["DATABASE_URL"],
-  },
-});
+import ws from "ws";
+neonConfig.webSocketConstructor = ws;
+const connectionString =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined in environment variables");
+}
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+const prisma = new PrismaClient({ adapter });
+export default prisma;
+
+// export default defineConfig({
+
+// schema: "prisma/schema.prisma",
+// migrations: {
+//   path: "prisma/migrations",
+// },
+
+// datasource: {
+//   url: process.env["DATABASE_URL"],
+// },
+// });
