84483b84946b7f9828baa44a9db43cacdbcd632c
OjogoMercy <mercyojogo@gmail.com>
2026-05-26 20:47:52 +0100

    refactor: update Prisma configuration and client initialization for improved database connection handling

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,32 +1,13 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { neonConfig, Pool } from "@neondatabase/serverless";
-import { PrismaClient } from "@prisma/client";
 
-import ws from "ws";
-neonConfig.webSocketConstructor = ws;
-const connectionString =
-  process.env.NODE_ENV === "test"
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL;
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined in environment variables");
-}
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
 
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-const prisma = new PrismaClient({ adapter });
-export default prisma;
-
-// export default defineConfig({
-
-// schema: "prisma/schema.prisma",
-// migrations: {
-//   path: "prisma/migrations",
-// },
-
-// datasource: {
-//   url: process.env["DATABASE_URL"],
-// },
-// });
+  datasource: {
+    url: process.env["DATABASE_URL"],
+  },
+});
--- prismaClient.js
+++ prismaClient.js
@@ -1,7 +1,17 @@
-const { PrismaClient } = require("@prisma/client");
-const { PrismaNeon } = require("@prisma/adapter-neon");
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig, Pool } from "@neondatabase/serverless";
+import { PrismaClient } from "@prisma/client";
+import ws from "ws";
+neonConfig.webSocketConstructor = ws;
+const connectionString =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined in environment variables");
+}
 
-const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
 const prisma = new PrismaClient({ adapter });
-
-module.exports = prisma;
+export default prisma;
