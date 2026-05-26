a7b0957f4bb42466317b6cf5da090b91634516b8
OjogoMercy <mercyojogo@gmail.com>
2026-05-26 21:52:42 +0100

    fix: update babel.config.js to use CommonJS module syntax and modify test script in package.json to run Jest

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
-{
-  "presets":[
-    ["@babel/preset-env", {
-      "targets": { "node": "current" }
-    }]
-  ]
-}
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: { node: "current" },
+      },
+    ],
+  ],
+};
